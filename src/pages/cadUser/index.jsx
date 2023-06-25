6c5a12e0d211721308cb7bd92875d12b0288df99
Rodrigo Fantibon <rodrigo.fantibon@hotmail.com>
2023-06-25 05:56:30 -0300

    ajuste layout pagina de cadastro

--- src/pages/cadUser/index.jsx
+++ src/pages/cadUser/index.jsx
@@ -1,8 +1,12 @@
 import { Header } from '../../components/header/header';
 import { Footer } from '../../components/footer/footer';
-import './index.css';
+// import './index.css';
 import Inputmask from 'inputmask';
 import { useEffect } from 'react';
+import logo from '../../imgs/logo.jpg';
+import styles from './index.module.css';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export function CadUser() {
   useEffect(() => {
@@ -10,54 +14,108 @@ export function CadUser() {
     Inputmask("999.999.999-99").mask("#cpf");
   }, []);
 
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isCPFHovered, setIsCPFHovered] = useState(false);
+  const [isPhoneHovered, setIsPhoneHovered] = useState(false);
+
   return (
-    <div className="darkMode">
+    <div className={styles.darkMode}>
       <Header />
-      <div className="content">
-        <div className="container_form formatForm" >
-          <h1>Formulário de Cadastro</h1>
-          <form className="form" action="#" method="post">
-            <div className="form_grupo">
-              <label htmlFor="nome" className="form_label">Nome</label>
-              <input type="text" name="nome" className="form_input" id="nome" placeholder="Nome" required/>
-            </div>
-            
-            <div className="form_grupo">
-              <label htmlFor="e-mail" className="form_label">Email</label>
-              <input type="email" name="email" className="form_input" id="email" placeholder="seuemail@example.com" required/>
-            </div>
-            
-            <div className="form_grupo">
-              <label htmlFor="datanascimento" className="form_label">Data de Nascimento</label>
-              <input type="date" name="datanascimento" className="form_input" id="datanascimento" placeholder="Data de Nascimento" required/>
-            </div>   
-            
-            <div className="form_grupo">
-              <label htmlFor="cpf" className="form_label">CPF</label>
-              <input type="text" name="cpf" className="form_input" id="cpf" placeholder="000.000.000-00" required/>
-            </div>     
-            
-            <div className="form_grupo">
-              <label htmlFor="phone" className="form_label">Telefone</label>
-              <input
-                type="text"
-                name="phone"
-                className="form_input"
-                id="phone"
-                placeholder="(11) 9-9999-9999"
-                required
-              />
-            </div>
-            
-            <div className="form_grupo">
-              <div className="submit">
-                <button type="submit" className="submit_btn">Cadastrar</button>
-              </div>   
-            </div>
-          </form>
+      <div className={styles.containerMain}>
+        <div className={styles.containerLogin}>
+          <div className={styles.wrapLogin}>
+            <form className={styles.loginForm}>
+              <span className={styles.loginFormTitle}>
+                <img className={styles.img} src={logo} alt="login" />Cadastre-se!
+              </span>
+
+              <div className={styles.wrapInput}>
+                <input
+                  className={`${name !== "" ? styles.hasVal : ""} ${styles.inputs}`}
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <span className={styles.focusInput} data-placeholder="Nome Completo"></span>
+              </div>
+
+              <div className={styles.wrapInput}>
+                <input
+                  className={`${phone !== "" ? styles.hasVal : ""} ${styles.inputs}`}
+                  type="text"
+                  id="phone"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  onMouseEnter={() => setIsPhoneHovered(true)}
+                  onMouseLeave={() => setIsPhoneHovered(false)}
+                />
+                <span
+                  className={`${styles.focusInput} ${isPhoneHovered || phone !== "" ? styles.focused : ""}`}
+                  data-placeholder="Telefone"
+                ></span>
+              </div>
+
+              <div className={styles.wrapInput}>
+                <input
+                  className={`${cpf !== "" ? styles.hasVal : ""} ${styles.inputs}`}
+                  type="text"
+                  id="cpf"
+                  value={cpf}
+                  onChange={(e) => setCpf(e.target.value)}
+                  onMouseEnter={() => setIsCPFHovered(true)}
+                  onMouseLeave={() => setIsCPFHovered(false)}
+                />
+                <span
+                  className={`${styles.focusInput} ${isCPFHovered || cpf !== "" ? styles.focused : ""}`}
+                  data-placeholder="CPF"
+                ></span>
+              </div>
+
+              <div className={styles.wrapInput}>
+                <input
+                  className={`${email !== "" ? styles.hasVal : ""} ${styles.inputs}`}
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <span className={styles.focusInput} data-placeholder="Email"></span>
+              </div>
+
+              <div className={styles.wrapInput}>
+                <input
+                  className={`${password !== "" ? styles.hasVal : ""} ${styles.inputs}`}
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <span className={styles.focusInput} data-placeholder="Password"></span>
+              </div>
+
+              <div className={styles.wrapInput}>
+                <input
+                  className={`${confirmPassword !== "" ? styles.hasVal : ""} ${styles.inputs}`}
+                  type="password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                <span className={styles.focusInput} data-placeholder="Confirm Password"></span>
+              </div>
+
+              <div className={styles.containerLoginFormBtn}>
+                <Link to="/FirstPage" className={styles.loginFormBtn}>
+                  <span>Register</span>
+                </Link>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
       <Footer />
     </div>
-  )
+  );
 }
