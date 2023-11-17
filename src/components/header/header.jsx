3c01e1099c4c054fc3454ddbf685ae26ef1adc77
Rodrigo Fantibon <rodrigo.fantibon@hotmail.com>
2023-11-17 00:04:23 -0300

    ajustes rotas

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,7 +5,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Dropdown from 'react-bootstrap/Dropdown'; // Importando o componente Dropdown
+import Dropdown from 'react-bootstrap/Dropdown';
 
 export function Header() {
     return (
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,14 +4,8 @@ import { Sidebar } from '../../components/sidebar/sidebar'
 import styles from './index.module.css'
 import logo from '../../imgs/logo.jpg';
 import backgroundImage from '../../imgs/background.png';
+import { Link } from 'react-router-dom'
 
-function redirectToCadOng() {
-  window.location.href = '/cadOng';
-}
-
-function redirectToCadUser() {
-  window.location.href = '/cadUser';
-}
 
 export function Home() {
   return (
@@ -29,13 +23,19 @@ export function Home() {
             <h1 className={styles.subTitle}>Deseja adotar um pet ? </h1>
             <h5 className={styles.center}>O lugar perfeito para encontrar seu novo amiguinho de quatro patas!</h5>
             <h6 className={styles.ctaText}>Cadastre-se agora para explorar todas as opções de adoção conhecer a nossa plataforma e encontrar o animal perfeito para você!</h6>
-            <button onClick={redirectToCadUser} className={styles.ctaButton}>Cadastre-se</button>
+            <Link to="/cadUser">
+            <button className={styles.ctaButton}>Cadastre-se</button>
+            </Link>
+            
           </div>
           <div className={styles.ongSection}>
             <h2 className={styles.subTitle}>Você faz parte de uma ONG?</h2>
             <h5 className={styles.center}>Na Tin Animal, também ajudamos organizações a encontrarem lares amorosos para animais que precisam.</h5>
             <h6 className={styles.ctaText}>Junte-se a nós para conheça a plataforma e conectar seus amiguinhos peludos a lares amorosos!</h6>
-            <button onClick={redirectToCadOng} className={styles.ctaButton}>Cadastre-se</button>
+            <Link to="/cadOng">
+            <button className={styles.ctaButton}>Cadastre-se</button>
+            </Link>
+           
           </div>
         </div>
         <Footer />
