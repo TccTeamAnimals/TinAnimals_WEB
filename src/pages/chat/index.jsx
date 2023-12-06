d26bab7e68051b9efb22ef2636f3f49e99f8b5cb
Rodrigo Fantibon <rodrigo.fantibon@hotmail.com>
2023-12-06 02:02:04 -0300

    ajuste page chat

--- src/pages/chat/index.jsx
+++ src/pages/chat/index.jsx
@@ -1,16 +1,23 @@
 import { Sidebar } from '../../components/sidebar/sidebar.jsx'
 import { ChatComponent } from '../../components/chat/chat.jsx'
-import { useContext } from "react"
+import { useContext, useEffect, useState} from "react"
 import { ThemeContext } from "../../contextApi/ThemeContext"
 import styles from "./index.module.css"
 
 export function Chat () {
-    const { darkMode } = useContext(ThemeContext);
+    const { darkMode, getLocalStorage} = useContext(ThemeContext);
+    const [ usuario, setUsuario ] = useState({});
+
+    useEffect(() => {
+        setUsuario(getLocalStorage());
+        console.log("USEEEEEEEEEEEEEEEEEER", usuario)
+    }, [])
+
     return (
         <div>
             <div className={`${darkMode ? styles.dark_mode : styles.light_mode}`}>
                 <div className={styles.container}>
-                    <h1 className={styles.page_title}>Chat</h1>
+                    <h1 className={styles.page_title}>Chat {usuario.typeCad ? 'Usuário' : 'ONG'} </h1>
                     <ChatComponent/>
                 </div>
             </div>
